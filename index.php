a9af4a7779542386f3335351fdbeb05ee8d169a8
Ian Leong <ileong@gmail.com>
2014-05-07 19:44:27 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title><?=htmlspecialchars($_SERVER["HTTP_HOST"])?></title>
+<style type="text/css">
+<!--
+body {
+	background-color: #CFCBC7;
+	margin: 0px;
+	padding: 0px;
+	background-image: url(/icons/i/bg.gif);
+	background-repeat: repeat-x;
+}
+.t {
+	font-family: Tahoma, Verdana, Arial, Helvetica, sans-serif;
+	font-size: 16px;
+	font-weight: bold;
+	color: #FF7C1F;
+	text-decoration: none;
+	background-image: url(/icons/i/tbg.gif);
+	background-repeat: repeat-x;
+	padding-left: 10px;
+	padding-right: 10px;
+}
+-->
+</style>
+</head>
+<body>
+
+<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-5PBLLP"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5PBLLP');</script>
+<!-- End Google Tag Manager -->
+
+<table border="0" cellspacing="0" cellpadding="0" align="center">
+<tr>
+<td colspan="3"><img src="/icons/i/t.gif" width="1" height="158" alt="" /></td>
+</tr>
+<tr>
+<td colspan="3" align="center"><img src="/icons/i/got.gif" alt="Got New Host" width="272" height="46" /></td>
+</tr>
+<tr>
+<td><img src="/icons/i/left.gif" width="14" height="60" alt="" /></td>
+<td align="center" width="243" nowrap="nowrap" class="t"><?=htmlspecialchars($_SERVER["HTTP_HOST"])?></td>
+<td><img src="/icons/i/right.gif" width="15" height="60" alt="" /></td>
+</tr>
+</table>
+</body>
+</html>
